3954170a4af69547e837feca6e3ce503ffd79473
jennmb <jennifer0615berna@gmail.com>
2017-03-08 16:53:03 -0800

    Add files via upload
    ex48 is on advance user input and creating a scanner

---
+++ lexicon.rb
@@ -0,0 +1,13 @@
+module Lexicon
+
+	def Lexicon.scan(sentence)
+		# break up the sentence into words
+		words = sentence.split(' ')
+		# for each word
+			# if it's number make it a number
+			# it it's a word, find out what kind
+			# otherwise, it's an error
+		return [['direction', 'north']]
+	end
+
+end

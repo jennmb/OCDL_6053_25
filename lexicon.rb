ef5ef0460b869d1fdd5ac0672f6d98e04c605942
jennmb <jennifer0615berna@gmail.com>
2017-03-15 16:58:41 -0700

    Add files via upload
    ex 48 
    advance user input

--- lexicon.rb
+++ lexicon.rb
@@ -4,10 +4,17 @@ module Lexicon
 		# break up the sentence into words
 		words = sentence.split(' ')
 		# for each word
+		words.each {|item| 
+			if item =~ /\A[0-9]+\z/
+				item.to_i
+			elsif item == 
+				puts " go #{item}."
+			else
+				puts "error"
+			end }
 			# if it's number make it a number
 			# it it's a word, find out what kind
 			# otherwise, it's an error
-		return [['direction', 'north']]
 	end
 
 end
